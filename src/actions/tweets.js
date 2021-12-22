b7665db59150279426992a0d4dd5846e05a45657
Nailcan Küçük <nailcankucuk@gmail.com>
2021-12-22 12:59:34 +0300

    New Tweet and Tweet Detail pages added

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,23 +1,42 @@
-import {saveLikeToggle} from "../utils/api";
+import {saveLikeToggle, saveTweet} from "../utils/api";
+import {showLoading, hideLoading} from "react-redux-loading";
 
 export const RECEIVE_TWEET = 'RECEIVE_TWEET'
 export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+export const ADD_TWEET = 'ADD_TWEET'
 
 export function receiveTweets(tweets) {
     return {
-        type: RECEIVE_TWEET,
-        tweets
+        type: RECEIVE_TWEET, tweets
     }
 }
 
 function toggleTweet({id, authedUser, hasLiked}) {
     return {
-        type: TOGGLE_TWEET,
-        id,
-        authedUser,
-        hasLiked
+        type: TOGGLE_TWEET, id, authedUser, hasLiked
     }
 }
+
+function addTweet(tweet) {
+    return {
+        type: ADD_TWEET, tweet
+    }
+}
+
+export const handleAddTweet = (text, replyingTo) => {
+    return (dispatch, getState) => {
+        dispatch(showLoading());
+        const { authedUser } = getState();
+    
+        return saveTweet({
+            text: text.text,
+            author: authedUser.authedUser,
+            replyingTo,
+        })
+            .then((tweet) => dispatch(addTweet(tweet)))
+            .then(() => dispatch(hideLoading()));
+    };
+};
 
 export function handleToggleTweet(info) {
     return (dispatch) => {
